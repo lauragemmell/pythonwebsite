61c5fc4979b31f2d77a502d908528d376b8528c3
Laura Gemmell <laura-gemmell@hotmail.com>
2015-03-21 18:48:48 +0000

    LG Update 21 March

--- hello.py
+++ hello.py
@@ -29,7 +29,7 @@ def company_sign_in():
 def company_sign_in_success():
 	return render_template('companysignin_successful.html')
 
-@app.route("/complete",methods=['POST'])
+@app.route("/company/signup/complete",methods=['POST'])
 def company_sign_up():
 	form_data = request.form
 	mandrill_client = mandrill.Mandrill('9FdJjzfupPJojkDcNiv4jA')
@@ -44,6 +44,31 @@ def company_sign_up():
 )
 	return render_template('companiessignedup.html')
 
+@app.route("/applicant/signup")
+def applicant_signup():
+	return render_template('applicant_signup.html')
 
+@app.route("/applicant/signup/complete", methods=['POST'])
+def sign_up():
+	form_data = request.form
+
+	email= form_data['email']
+	name= form_data['name']
+
+	mandrill_client.messages.send(
+	message={
+		'subject': 'Thanks for signing up',
+		'from_email': 'chayseldenashby@gmail.com',
+		'to': [
+			{
+				'email': email
+			}
+		],
+		'html': '<p> Welcome to JobSwipe, {0}. You are now ready to take the next step to your dream job </p>'.format(name)
+	},
+	async=False
+	)
+
+	return "Thanks for signing up. Please check your inbox for your confirmation email"
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000,debug=True)
