c02496f432d2a8a20612c136c837eb5a9d29a687
Camilla Hayselden-Ashby <chayseldenashby@gmail.com>
2015-03-31 18:47:24 +0100

    Created Job generator

--- hello.py
+++ hello.py
@@ -99,6 +99,64 @@ def applicant_signin():
 @app.route("/applicant/interface")
 def applicant_interface():
 	return render_template('applicant_interface.html')
+
+@app.route("/applicant/jobinator")
+def job_gen():
+	return render_template('job_gen.html')
+
+@app.route("/applicant/jobinator/result", methods=['POST'])
+def job_gen_result():
+	form_data = request.form
+	Enjoy = form_data['Enjoy']
+	Interests = form_data['Interests']
+	Vibe = form_data['Vibe']
+	Colour = form_data['Colour']
+	Animal = form_data['Animal']
+
+	if Interests == "Gaming":
+		Company="mobile gaming company"
+	elif Interests == "Fashion":
+		Company="trend analysis start up"
+	elif Interests == "Nature":
+		if Enjoy == "Active":
+			Company = "marine conservation unit"
+		else: 
+			Company = "enviromental NGO"
+	elif Interests == "Sport":
+		Company = "sponsorship valuation agency"
+	elif Interests == "Film":
+		Company = "a YouTube talent management company"
+	else:
+		Company="start up"
+
+	if Enjoy == "Active":
+		role = "field worker"
+	else:
+		role = Enjoy
+
+	if Vibe == "chilled":
+		if Enjoy == "Active":
+			Office = "with its offices outside the city"
+		else:
+			Office = "with big, airy, open-plan offices"
+	elif Vibe == "freedom":
+		if Enjoy == "graphic designer":
+			Office = "where you can work from wherever makes you feel inspired"
+		elif Enjoy == "support team worker":
+			Office = "where you can fit your work around your other commitments"	
+		else:
+			Office = "where you can work from wherever you want and to your own schedule"
+	elif Vibe == "busy":
+		Office = "where there is always something going on"
+	elif Vibe == "social":
+		if Interests == Sport:
+			Office = "where there is a company football team"
+		else:
+			Office = "with a great office culture"
+	elif Vibe == "pressure":
+		Office = "where there is always plenty of work to keep busy with"
+	
+	return render_template('job_gen_result.html', Company=Company, role=role, Office=Office, Animal=Animal)
 		
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000,debug=True)
